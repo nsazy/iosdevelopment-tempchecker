fdcb58035eabd97afedbc42977700db44fd4b3a4
Nick Sazy <nrsazy@yahoo.com>
2022-07-03 17:18:38 -0400

    Initial Commit

---
+++ TempChecker/TempCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempCheckerApp.swift
+//  TempChecker
+//
+//  Created by Nicholas Sazy on 7/3/22.
+//
+
+import SwiftUI
+
+@main
+struct TempCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
